f76d94d4b14890e0e15eced3417fb4020381022e
MS-M <m.siemieniukmorawski@gmail.com>
2017-06-04 21:23:51 +0200

    finish section about

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,8 +9,8 @@ document.addEventListener("DOMContentLoaded", function() {
         responsiveClass: true,
         items: 1,
         loop: true,
-        nav:false,
-        dots:true,
+        nav: false,
+        dots: true,
     });
     $('.owl__html').owlCarousel({
         loop: true,
@@ -18,22 +18,103 @@ document.addEventListener("DOMContentLoaded", function() {
         responsiveClass: true,
         items: 1,
         loop: true,
-        nav:false,
-        dots:true,
+        nav: false,
+        dots: true,
     })
-$('.owl__email').owlCarousel({
+    $('.owl__email').owlCarousel({
         loop: true,
         margin: 10,
         responsiveClass: true,
         items: 1,
         loop: true,
-        nav:false,
-        dots:true,
+        nav: false,
+        dots: true,
     })
-
 
 });
 
+
+// ——————————————————————————————————————————————————
+// link scroll
+// ——————————————————————————————————————————————————
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+$('.close').click(function() {
+    $('.half--about').removeClass("show-up");
+    $('.half--contact').removeClass("show-down");
+    $('body').removeClass("overflow--hide");
+    setTimeout(function() {
+        $('.container__about__contact ').removeClass("zindex");
+    }, 500);
+
+})
+$('.about').click(function() {
+    event.preventDefault();
+    $('.half--about').addClass("show-up");
+    $('.half--contact').addClass("show-down");
+    $('body').addClass("overflow--hide");
+    $('.container__about__contact ').addClass("zindex");
+
+});
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        if (hasClass(this, "about") || hasClass(this, "contact")) {
+            event.preventDefault();
+            $('.half--about').addClass("show-up");
+            $('.half--contact').addClass("show-down");
+            $('body').addClass("overflow--hide");
+            $('.container__about__contact ').addClass("zindex");
+        } else {
+
+
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[class=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        }
+    });
+
+
+// ——————————————————————————————————————————————————
+//show button up
+// ——————————————————————————————————————————————————
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 400) {
+        $('.button__top').fadeIn();
+    } else {
+        $('.button__top').fadeOut();
+    }
+
+});
 
 
 
@@ -55,8 +136,8 @@ class TextScramble {
         for (let i = 0; i < length; i++) {
             const from = oldText[i] || ''
             const to = newText[i] || ''
-            const start = Math.floor(Math.random() * 50)
-            const end = start + Math.floor(Math.random() * 200)
+            const start = Math.floor(Math.random() * 20)
+            const end = start + Math.floor(Math.random() * 50)
             this.queue.push({ from, to, start, end })
         }
         cancelAnimationFrame(this.frameRequest)
